47cf450117d2df234af3f50177630296400ebe92
Colin Horner <25807014+cmcolinh@users.noreply.github.com>
2019-12-28 23:59:41 +0900

    [Mod] add some of the methods that describe a CharacterCreateMessage

--- src/main/java/com/openeqoa/server/network/udp/in/packet/message/CharacterCreateMessage.java
+++ src/main/java/com/openeqoa/server/network/udp/in/packet/message/CharacterCreateMessage.java
@@ -8,6 +8,8 @@ import com.openeqoa.server.network.udp.MessageHandler;
  *
  */
 public final class CharacterCreateMessage extends AbstractPacketWrappingClientMessage {
+	private int nameLength = 0;
+
 	public CharacterCreateMessage(byte[] wrappedPacketBytes, int startIndex, int length) {
 		super(wrappedPacketBytes, startIndex, length);
 	}
@@ -15,5 +17,63 @@ public final class CharacterCreateMessage extends AbstractPacketWrappingClientMe
 	@Override
 	public void accept(MessageHandler handler) {
 		handler.visit(this);
+	}
+
+	/** @return the length of the selected name */
+	public int getNameLength() {
+		return nameLength > 0 ? nameLength : wrappedPacketBytes[startIndex + 5] << 24
+				+ wrappedPacketBytes[startIndex + 4] << 16
+				+ wrappedPacketBytes[startIndex + 3] << 8
+				+ wrappedPacketBytes[startIndex + 2];
+	}
+
+	/** @return the selected player name */
+	public String getName() {
+		String name = "";
+		for (int index = startIndex + 6; index < startIndex + 6 + getNameLength(); index++) {
+			name += wrappedPacketBytes[index];
+		}
+		return name;
+	}
+
+	/** @return the player's level (should be 1, right?) */
+	public byte getPlayerLevel() {
+		//packets store level as double it's actual value, so will divide by 2
+		return (byte)(wrappedPacketBytes[startIndex + getNameLength() + 6] / 2);
+	}
+
+	/** @return the selected race */
+	public byte getPlayerRace() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 7];
+	}
+
+	/** @return the selected class */
+	public byte getPlayerClass() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 8];
+	}
+
+	/** @return the selected gender (internally, 0x00 is male and 0x02 is female) */
+	public byte getPlayerGender() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 9];
+	}
+
+	/** @return the selected hair color */
+	public byte getPlayerHairColor() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 10];
+	}
+
+	/** @return the selected hair style */
+	public byte getPlayerHairStyle() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 11];
+	}
+
+	/** @return the selected hair length */
+	public byte getPlayerHairLength() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 12];
+	}
+
+	/** @return the selected face */
+	public byte getPlayerFace() {
+		return wrappedPacketBytes[startIndex + getNameLength() + 12];
 	}
 }
